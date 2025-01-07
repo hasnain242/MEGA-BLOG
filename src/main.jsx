28a22d2278ac31cf456050c25ae491d4a4c32fcd
hasnain sadiq <78303526+hasnain242@users.noreply.github.com>
2025-01-07 22:28:36 +0500

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -28,7 +28,7 @@ const router = createBrowserRouter([
             element: (
                 <Authlayout authentication={false}>
                     <Login />
-                </AuthLayout>
+                </Authlayout>
             ),
         },
         {
